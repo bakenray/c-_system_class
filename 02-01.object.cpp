0a24d5bbe534a94f4d495b5a4987ba3c033ef2f2
bakenray <lpqstrong@163.com>
2022-09-16 11:29:59 +0800

    add: object temp

---
+++ 02-01.object.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 02-01.object.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 16 Sep 2022 11:28:23 AM CST
+ ************************************************************************/
+
+#include<iostream>
+
+int main() {
+    int x;
+    x = 3;
+    std::cout << "Hello World" << std::endl;
+    std::cout << x << std::endl;
+    std::cout << (x + 1) <<std::endl;
+    return 0;
+}
