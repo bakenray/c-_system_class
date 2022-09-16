7584ab90b7a05097afd476c8efcb5a5849f193aa
bakenray <lpqstrong@163.com>
2022-09-16 11:22:35 +0800

    add: helloworld

---
+++ 01.helloworld.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 01.helloworld.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 16 Sep 2022 11:21:24 AM CST
+ ************************************************************************/
+
+#include<iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
