4c840e420ba74f3098f97d2edb99d74a6090d678
bakenray <lpqstrong@163.com>
2022-09-19 10:57:08 +0800

    add: ouput

---
+++ 05-01.ouput.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 05-01.ouput.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 19 Sep 2022 10:55:33 AM CST
+ ************************************************************************/
+
+#include<iostream>
+
+int main() {
+    int x = 6;
+    std::string s = "Hello";
+    float f = 1.234;
+    std::cout << x << " " << s << "\t" << f << std::endl;
+}
