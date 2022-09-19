0f769c50ca7aa618c7dda449df516d2113518c32
bakenray <lpqstrong@163.com>
2022-09-19 11:04:31 +0800

    add: error

---
+++ 03-04.error.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 03-04.error.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 19 Sep 2022 11:02:28 AM CST
+ ************************************************************************/
+
+#include<iostream>
+
+int main() {
+    std::cerr << "error" << std::endl;
+    std::cout << "ouput" << std::endl;
+}
